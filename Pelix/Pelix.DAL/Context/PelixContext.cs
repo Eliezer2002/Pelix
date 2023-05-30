478bb5a91bed3381a71388803f87463b28052b26
MSI GL65 <MSI GL65@MSI>
2023-05-29 23:06:55 -0400

    Eliminando Capa Api

---
+++ Pelix/Pelix.DAL/Context/PelixContext.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Pelix.DAL.Entities;
+
+namespace Pelix.DAL.Context
+{
+    public class PelixContext : DbContext
+    {
+        public PelixContext()
+        {
+
+        }
+        //public DbSet<Pelicula> peliculas { get; set; }
+        public DbSet<Genero> generos { get; set; }
+        public DbSet<GeneroPelicula> generoPeliculas { get; set; }
+        public DbSet<Rent> rents { get; set; }
+        public DbSet<Rol> rols { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<Sale> sales { get; set; }
+        public PelixContext(DbContextOptions<PelixContext> options) : base(options)
+        {
+        }
+        #region "Registros"
+        public DbSet<Pelicula> peliculas { get; set; }
+        #endregion
+    }
+}
---
+++ Pelix/Pelix.DAL/Repositories/PeliculaRepository.cs
@@ -0,0 +1,70 @@
+﻿
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using Pelix.DAL.Context;
+using Pelix.DAL.Entities;
+using Pelix.DAL.Exception;
+using Pelix.DAL.Interfaces;
+using Pelix.DAL.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pelix.DAL.Repositories
+{
+    public class PeliculaRepository : IPeliculaRepository
+    {
+        private readonly PelixContext context;
+        private readonly ILogger logger;
+
+        public PeliculaRepository(PelixContext context, ILogger<PeliculaRepository> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+        public List<PeliculaModel> GetAll()
+        {
+            return new List<PeliculaModel>()
+            {
+                new PeliculaModel() 
+                {   txt_desc= "Hola",
+                    cant_disponibles_alquiler=2,
+                    cant_disponibles_venta=1,
+                    precio_alquiler=3.2,
+                    precio_venta= 15.2
+                }
+            };
+        }
+
+        public Pelicula GetbyId(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(Pelicula pelicula)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(Pelicula pelicula)
+        {
+            try
+            {
+                if (context.peliculas.Any(cd => cd.cod_pelicula == pelicula.cod_pelicula)) 
+                {
+                    throw new PeliculaException("La Pelicula Ya Existe!");
+                }
+                context.peliculas.Add(pelicula);
+                context.SaveChanges();
+            }
+            catch (System.Exception ex)
+            {
+                logger.LogError($" Ocurrio un error {ex.Message}", ex.ToString());
+            }
+        }
+
+        public void Update(Pelicula pelicula)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
